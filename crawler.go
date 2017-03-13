38257f67cc38021f54c8fa69f3d5fbfaa41cf5f0
RideWindX <ridewindx@163.com>
2017-03-14 00:10:39 +0800

    Update

--- crawler.go
+++ crawler.go
@@ -9,27 +9,14 @@ type Config struct {
 	*viper.Viper
 }
 
-type CrawlerRunner struct {
+type Crawler struct {
 	*Config
 	*logrus.Logger
-
-	crawlers []Crawler
 }
 
-func (cr *CrawlerRunner) Run() {
+func (c *Crawler) Run() {
 	// TODO: signal handling for SIGTERM, SIGINT, SIGBREAK
-	//
 }
 
-func (cr *CrawlerRunner) Stop() {
-	for _, c := range cr.crawlers {
-		go c.stop()
-	}
-}
-
-type Crawler struct {
-
-}
-
-func (c *Crawler) stop() {
+func (cr *Crawler) Stop() {
 }
