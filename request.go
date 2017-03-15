a9159d5247be8e4a50fa4c297bc3943cbb8dd857
RideWindX <ridewindx@163.com>
2017-03-15 17:38:15 +0800

    Update request.go

--- request.go
+++ request.go
@@ -7,3 +7,15 @@ type Request struct {
 
 	Meta map[string]interface{}
 }
+
+func NewRequest(urlStr, method string) (*Request, error) {
+	req, err := http.NewRequest(method, urlStr, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		Request: req,
+		Meta: make(map[string]interface{}),
+	}, nil
+}
