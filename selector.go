741ad16a3e6a7380395f5c43084c83ef32a03210
RideWindX <ridewindx@163.com>
2017-03-16 17:19:36 +0800

    Create selector.go

---
+++ selector.go
@@ -0,0 +1,106 @@
+package spy
+
+import (
+	"regexp"
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Selector interface {
+	Select(query string) Selectors
+	Regex(regex interface{}) []string
+	Extract() string
+}
+
+type Selectors []Selector
+
+func (ss Selectors) Select(query string) Selectors {
+	var result Selectors
+	for _, s := range ss {
+		result = append(result, s.Select(query)...)
+	}
+	return result
+}
+
+func (ss Selectors) Regex(regex interface{}) []string {
+	re := getRegex(regex)
+
+	var result []string
+	for _, s := range ss {
+		result = append(result, s.Regex(re)...)
+	}
+	return result
+}
+
+func (ss Selectors) RegexFirst(regex interface{}) string {
+	re := getRegex(regex)
+
+	for _, s := range ss {
+		for _, item := range s.Regex(re) {
+			return item
+		}
+	}
+	return ""
+}
+
+func (ss Selectors) Extract() []string {
+	var result []string
+	for _, s := range ss {
+		result = append(result, s.Extract())
+	}
+	return result
+}
+
+func (ss Selectors) ExtractFirst() string {
+	for _, s := range ss {
+		return s.Extract()
+	}
+	return ""
+}
+
+type GoquerySelector struct {
+	*goquery.Selection
+}
+
+func NewGoquerySelector(doc *goquery.Document) *GoquerySelector {
+	return &GoquerySelector{
+		Selection: doc.Selection,
+	}
+}
+
+func (gs *GoquerySelector) Select(query string) Selectors {
+	var result = make(Selectors, gs.Length())
+	gs.Find(query).Each(func(i int, s *goquery.Selection) {
+		result = append(result, s)
+	})
+	return result
+}
+
+func (gs *GoquerySelector) Regex(regex interface{}) []string {
+	re := getRegex(regex)
+
+	var result []string
+	for _, slice := range re.FindAllStringSubmatch(gs.Extract(), -1) {
+		if len(slice) == 1 {
+			result = append(result, slice[0]) // no submatch
+		} else {
+			result = append(result, slice[1:]...) // exclude the whole match
+		}
+	}
+	return result
+}
+
+func (gs *GoquerySelector) Extract() string {
+	// TODO: html.UnescapeString
+	return gs.Text()
+}
+
+func getRegex(regex interface{}) *regexp.Regexp {
+	switch re := regex.(type) {
+	case *regexp.Regexp:
+		return re
+	case string:
+		return regexp.MustCompile(re)
+	default:
+		panic("regex type must be either *regexp.Regexp or string")
+	}
+}
