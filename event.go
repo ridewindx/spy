fe463993c17f02dd5c7e071322cd6364faf94525
RideWindX <ridewindx@163.com>
2017-03-26 13:24:56 +0800

    Update

---
+++ event.go
@@ -0,0 +1,40 @@
+package spy
+
+import (
+	"github.com/asaskevich/EventBus"
+)
+
+var bus = EventBus.New()
+
+type Event string
+
+const (
+	EngineStarted Event = "EngineStarted"
+	EngineStopped Event = "EngineStopped"
+	SpiderOpened Event = "SpiderOpened "
+	SpiderIdle Event = "SpiderIdle"
+	SpiderClosed Event = "SpiderClosed"
+	SpiderError Event = "SpiderError"
+	RequestScheduled Event = "RequestScheduled"
+	RequestDropped Event = "RequestDropped"
+	ResponseReceived Event = "ResponseReceived"
+	ResponseDownloaded Event = "ResponseDownloaded"
+	ItemScraped Event = "ItemScraped"
+	ItemDropped Event = "ItemDropped"
+)
+
+func (e Event) SubAsync(fn interface{}) {
+	panic(bus.SubscribeAsync(string(e), fn, false))
+}
+
+func (e Event) Sub(fn interface{}) {
+	panic(bus.Subscribe(string(e), fn))
+}
+
+func (e Event) Unsub(fn interface{}) {
+	panic(bus.Unsubscribe(string(e), fn))
+}
+
+func (e Event) Pub(args ...interface{}) {
+	bus.Publish(string(e), args...)
+}
