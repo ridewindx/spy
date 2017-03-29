665818c99682921aa8adac978784d98beeb45c35
RideWindX <ridewindx@163.com>
2017-03-29 19:27:31 +0800

    Update spider.go

--- spider.go
+++ spider.go
@@ -1,32 +1,35 @@
 package spy
 
-type Item struct {
-	*Response
-	item []string
+type Item map[string]interface{}
+
+type SpiderResult struct {
+	Requests []*Request
+	Items []*Item
+}
+
+func (sr *SpiderResult) Empty() bool {
+	return sr == nil || (len(sr.Requests) == 0 && len(sr.Items) == 0)
 }
 
 type ISpider interface {
-	startRequests() ([]*Request, error)
-	parseNext(response *Response) (*Item, error)
+	StartRequests() []*Request
+	Parse(response *Response) (*SpiderResult, error)
 }
 
 type Spider struct {
-	startURLs []string
+	Name      string
+	StartURLs []string
 }
 
-func (s *Spider) startResusts() ([]*Request, error) {
-	reqs := make([]*Request, 0, len(s.startURLs))
-	for i, urlStr := range s.startURLs {
-		req, err := NewRequest(urlStr, "")
-		if err != nil {
-			return nil, err
-		}
-		reqs[i] = req
+func (s *Spider) StartResusts() []*Request {
+	reqs := make([]*Request, len(s.StartURLs))
+	for i, url := range s.StartURLs {
+		reqs[i] = NewRequest(url, "")
 	}
-	return reqs, nil
+	return reqs
 }
 
-func (s *Spider) parseNext(response *Response) (*Item, error) {
+func (s *Spider) Parse(response *Response) (*SpiderResult, error) {
 	panic("not implemented")
 }
 
@@ -34,10 +37,107 @@ type CrawlSpider struct {
 	*Spider
 }
 
-func (s *CrawlSpider) parseNext(response *Response) (*Item, error) {
+func (s *CrawlSpider) Parse(response *Response) (*SpiderResult, error) {
 
 }
 
 type Rule struct {
 	
+}
+
+type SpiderMiddleware interface {}
+
+type SpiderInputProcessor interface {
+	ProcessSpiderInput(response *Response, spider ISpider) error
+}
+
+type SpiderOutputProcessor interface {
+	ProcessSpiderOutput(result *SpiderResult, response *Response, spider ISpider) (*SpiderResult, error)
+}
+
+type SpiderErrorProcessor interface {
+	ProcessSpiderError(err error, response *Response, spider ISpider) (*SpiderResult, error)
+}
+
+type StartRequestsProcessor interface {
+	ProcessStartRequests(startRequests []*Request, spider ISpider) ([]*Request, error)
+}
+
+type SpiderMiddlewareManager struct {
+	middlewares []SpiderMiddleware
+	spiderInputProcessors []SpiderInputProcessor
+	spiderOutputProcessors []SpiderOutputProcessor
+	spiderErrorProcessors []SpiderErrorProcessor
+	startRequestsProcessors []StartRequestsProcessor
+}
+
+func (smm *SpiderMiddlewareManager) Register(middleware SpiderMiddleware) {
+	smm.middlewares = append(smm.middlewares, middleware)
+
+	if p, ok := middleware.(SpiderInputProcessor); ok {
+		smm.spiderInputProcessors = append(smm.spiderInputProcessors, p)
+	}
+	if p, ok := middleware.(SpiderOutputProcessor); ok {
+		smm.spiderOutputProcessors = append([]SpiderOutputProcessor{p}, smm.spiderOutputProcessors)
+	}
+	if p, ok := middleware.(SpiderErrorProcessor); ok {
+		smm.spiderErrorProcessors = append([]SpiderErrorProcessor{p}, smm.spiderErrorProcessors)
+	}
+	if p, ok := middleware.(StartRequestsProcessor); ok {
+		smm.startRequestsProcessors = append([]StartRequestsProcessor{p}, smm.startRequestsProcessors)
+	}
+}
+
+func (smm *SpiderMiddlewareManager) ScrapeResponse(request *Request, response *Response, spider ISpider) (*SpiderResult, error) {
+	var result *SpiderResult
+	var err error
+
+	for _, processor := range smm.spiderInputProcessors {
+		err = processor.ProcessSpiderInput(response, spider)
+		if err != nil {
+			break
+		}
+	}
+
+	if err == nil {
+		if request.Callback != nil {
+			result, err = request.Callback(response, nil) // request callback handles response
+		} else {
+			result, err = spider.Parse(response) // spider parse response
+		}
+	} else if request.Callback != nil {
+		result, err = request.Callback(nil, err) // request callback handles error
+	}
+
+	if err != nil {
+		for _, processor := range smm.spiderErrorProcessors {
+			result, err = processor.ProcessSpiderError(err, response, spider)
+			if err != nil || result != nil {
+				break
+			}
+		}
+	}
+
+	if err == nil && result != nil {
+		for _, processor := range smm.spiderOutputProcessors {
+			result, err = processor.ProcessSpiderOutput(result, response, spider)
+			if err != nil {
+				break
+			}
+		}
+	}
+
+	return result, err
+}
+
+func (smm *SpiderMiddlewareManager) ProcessStartRequests(startRequests []*Request, spider ISpider) ([]*Request, error) {
+	var result = startRequests
+	var err error
+	for _, processor := range smm.startRequestsProcessors {
+		result, err = processor.ProcessStartRequests(startRequests, spider)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return result, err
 }
