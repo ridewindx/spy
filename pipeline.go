d07d7322b03137865db12d7ffef3d8e7895d1778
RideWindX <ridewindx@163.com>
2017-03-29 19:26:21 +0800

    Update pipeline.go

--- pipeline.go
+++ pipeline.go
@@ -1,20 +1,30 @@
 package spy
 
-type ItemPipelineManager struct {
-	*MiddlewareManager
+type ItemPipelineMiddleware interface{}
+
+type ItemProcessor interface {
+	ProcessItem(item *Item, spider ISpider) (*Item, error)
 }
 
-func NewItemPipelineManager() *ItemPipelineManager {
-	return &ItemPipelineManager{
-		MiddlewareManager: NewMiddlewareManager(),
+type ItemPipelineManager struct {
+	middlewares []ItemPipelineMiddleware
+	itemProcessors []ItemProcessor
+}
+
+func (ipm *ItemPipelineManager) Register(middleware ItemPipelineMiddleware) {
+	ipm.middlewares = append(ipm.middlewares, middleware)
+
+	if p, ok := middleware.(ItemProcessor); ok {
+		ipm.itemProcessors = append(ipm.itemProcessors, p)
 	}
 }
 
-func (ipm *ItemPipelineManager) Register(middleware Middleware) {
-	ipm.MiddlewareManager.Register(middleware)
-	ipm.addHandler(middleware, "ProcessItem")
-}
-
-func (ipm *ItemPipelineManager) ProcessItem(item *Item, spider ISpider) *Item {
-	return ipm.callHandler("ProcessItem", item, spider).(*Item)
+func (ipm *ItemPipelineManager) ProcessItem(item *Item, spider ISpider) (*Item, error) {
+	for _, processor := range ipm.itemProcessors {
+		item, err := processor.ProcessItem(item, spider)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return item, nil
 }
