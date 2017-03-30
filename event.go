04a326663c9fa242e60c1965bf0bf799a53cb966
RideWindX <ridewindx@163.com>
2017-03-30 20:45:02 +0800

    Update event.go

--- event.go
+++ event.go
@@ -9,18 +9,18 @@ var bus = EventBus.New()
 type Event string
 
 const (
-	EngineStarted Event = "EngineStarted"
-	EngineStopped Event = "EngineStopped"
-	SpiderOpened Event = "SpiderOpened "
-	SpiderIdle Event = "SpiderIdle"
-	SpiderClosed Event = "SpiderClosed"
-	SpiderError Event = "SpiderError"
-	RequestScheduled Event = "RequestScheduled"
-	RequestDropped Event = "RequestDropped"
-	ResponseReceived Event = "ResponseReceived"
+	CrawlerStarted     Event = "CrawlerStarted"
+	CrawlerStopped     Event = "CrawlerStopped"
+	SpiderOpened       Event = "SpiderOpened "
+	SpiderIdle         Event = "SpiderIdle"
+	SpiderClosed       Event = "SpiderClosed"
+	SpiderError        Event = "SpiderError"
+	RequestScheduled   Event = "RequestScheduled"
+	RequestDropped     Event = "RequestDropped"
+	ResponseReceived   Event = "ResponseReceived"
 	ResponseDownloaded Event = "ResponseDownloaded"
-	ItemScraped Event = "ItemScraped"
-	ItemDropped Event = "ItemDropped"
+	ItemScraped        Event = "ItemScraped"
+	ItemDropped        Event = "ItemDropped"
 )
 
 func (e Event) SubAsync(fn interface{}) {
