00b900e0e5135b2113ece7640570113abc7244b6
RideWindX <ridewindx@163.com>
2017-03-30 20:47:09 +0800

    Update pipeline.go

--- pipeline.go
+++ pipeline.go
@@ -14,14 +14,23 @@ type ItemPipelineManager struct {
 func (ipm *ItemPipelineManager) Register(middleware ItemPipelineMiddleware) {
 	ipm.middlewares = append(ipm.middlewares, middleware)
 
-	if p, ok := middleware.(ItemProcessor); ok {
-		ipm.itemProcessors = append(ipm.itemProcessors, p)
+	if processor, ok := middleware.(ItemProcessor); ok {
+		ipm.itemProcessors = append(ipm.itemProcessors, processor)
 	}
 }
 
+func (ipm *ItemPipelineManager) Open(spider ISpider) {
+	open(spider, ipm.middlewares...)
+}
+
+func (ipm *ItemPipelineManager) Close(spider ISpider) {
+	close(spider, ipm.middlewares...)
+}
+
 func (ipm *ItemPipelineManager) ProcessItem(item *Item, spider ISpider) (*Item, error) {
+	var err error
 	for _, processor := range ipm.itemProcessors {
-		item, err := processor.ProcessItem(item, spider)
+		item, err = processor.ProcessItem(item, spider)
 		if err != nil {
 			return nil, err
 		}
