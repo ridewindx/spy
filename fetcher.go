514f490a7bd5436ae8e3935e4f0ce5fa446d04ab
RideWindX <ridewindx@163.com>
2017-03-30 20:45:53 +0800

    Update fetcher.go

--- fetcher.go
+++ fetcher.go
@@ -9,7 +9,7 @@ import (
 )
 
 type IFetcher interface {
-	Fetch(req *Request, spider ISpider) (*FetchResult, error)
+	Fetch(req *Request, spider ISpider) (*Response, *Request, error)
 }
 
 type Fetcher struct {
@@ -27,7 +27,7 @@ type Fetcher struct {
 }
 
 type FetcherHandler interface {
-	Fetch(request *Request, spider ISpider) *Response
+	Fetch(request *Request, spider ISpider) (*Response, error)
 	Close()
 }
 
@@ -49,18 +49,11 @@ func NewFetcher() *Fetcher {
 	}
 }
 
-type FetchResult struct {
-	*Request
-	*Response
-	error
-}
-
 func (f *Fetcher) Fetch(req *Request, spider ISpider) (*FetchResult, error) {
 	f.active[req] = struct{}{}
 	defer delete(f.active, req)
 
-	// TODO
-	result := f.FetcherMiddlewareManager.Fetch(f.enqueueRequest, req, spider)
+	rep, req, err := f.FetcherMiddlewareManager.Fetch(f.enqueueRequest, req, spider)
 	if result.error != nil {
 		return nil, result.error
 	}
@@ -167,73 +160,130 @@ func (f *Fetcher) getSlot(req *Request) (key string, slot *Slot) {
 	return
 }
 
-func (fmr *FetchResult) Empty() bool {
-	return fmr == nil || (fmr.Request == nil && fmr.Response == nil && fmr.error == nil)
+type FetcherMiddleware interface {}
+
+type FetchingRequestProcessor interface {
+	/* ProcessRequest is called for each request that goes through the fetcher middleware.
+	It should either: return nils, return a Response object, return a Request object, or return an IgnoreRequest error.
+
+	If it returns nils, will continue processing this request, executing all other middlewares,
+	until, finally, the appropriate fetcher handler performs the request performed (and its response downloaded).
+
+	If it returns a Response object, won’t bother calling any other ProcessRequest or ProcessError methods,
+	or the appropriate fetcher handler; it’ll return that response. The ProcessResponse methods of installed middleware
+	is always called on every response.
+
+	If it returns a Request object, will stop calling ProcessRequest methods and reschedule the returned request.
+	Once the newly returned request is performed, the appropriate middleware chain will again be called on the downloaded response.
+
+	If it returns an IgnoreRequest error, the ProcessError methods of installed downloader middleware will be called.
+	If none of them handle the error, the callback function of the request (Request.Callback) is called.
+	If no code handles the returned error, it is ignored and not logged (unlike other error).
+	 */
+	ProcessRequest(request *Request, spider ISpider) (*Response, *Request, error)
 }
 
-type FetcherMiddleware interface {
-	Middleware
+type FetchingResponseProcessor interface {
+	/* ProcessResponse should either: return a Response object, return a Request object or return an IgnoreRequest error.
 
-	// One or more of the following methods should be implemented.
-	// ProcessRequest(request *Request, spider ISpider) *FetchResult
-	// ProcessResponse(response *FetchResult, spider ISpider) *FetchResult
-	// ProcessException(exception *FetchResult, spider ISpider) *FetchResult
+	If it returns a Response (it could be the same given response, or a brand-new one), that response will continue to be
+	processed with the ProcessResponse method of the next middleware in the chain.
+
+	If it returns a Request object, the middleware chain is halted and the returned request is rescheduled to be performed
+	in the future. This is the same behavior as if a request is returned from ProcessRequest.
+
+	If it returns an IgnoreRequest error, the callback function of the request (Request.Callback) is called.
+	If no code handles the returned error, it is ignored and not logged (unlike other error).
+	 */
+	ProcessResponse(response *Response, request *Request, spider ISpider) (*Response, *Request, error)
+}
+
+type FetchingErrorProcessor interface {
+	/* ProcessError is called when a fetcher handler or a ProcessRequest (from a fetcher middleware) returns an error.
+	It should return: either nils, a Response object, or a Request object.
+
+	If it returns nils, will continue processing this error, executing any other ProcessError methods of installed
+	middleware, until no middleware is left.
+
+	If it returns a Response object, the ProcessResponse methods chain of installed middleware is started,
+	and won’t bother calling any other ProcessError methods of middleware.
+
+	If it returns a Request object, the returned request is rescheduled to be performed in the future.
+	This stops the execution of ProcessError methods of the middleware the same as returning a response would.
+	 */
+	ProcessError(err error, request *Request, spider ISpider) (*Response, *Request)
 }
 
 type FetcherMiddlewareManager struct {
-	*MiddlewareManager
-}
-
-func NewFetcherMiddlewareManager() *FetcherMiddlewareManager {
-	return &FetcherMiddlewareManager{
-		MiddlewareManager: NewMiddlewareManager(),
-	}
+	middlewares []FetcherMiddleware
+	requestProcessors []FetchingRequestProcessor
+	responseProcessors []FetchingResponseProcessor
+	errorProcessors []FetchingErrorProcessor
 }
 
 func (fmm *FetcherMiddlewareManager) Register(middleware FetcherMiddleware) {
-	mw := Middleware(middleware)
-	fmm.MiddlewareManager.Register(mw)
-	fmm.addHandler(mw, "ProcessRequest")
-	fmm.addHandler(mw, "ProcessResponse", true)
-	fmm.addHandler(mw, "ProcessException", true)
+	fmm.middlewares = append(fmm.middlewares, middleware)
+
+	if p, ok := middleware.(FetchingRequestProcessor); ok {
+		fmm.requestProcessors = append(fmm.requestProcessors, p)
+	}
+	if p, ok := middleware.(FetchingResponseProcessor); ok {
+		fmm.responseProcessors = append([]FetchingResponseProcessor{p}, fmm.responseProcessors...)
+	}
+	if p, ok := middleware.(FetchingErrorProcessor); ok {
+		fmm.errorProcessors = append([]FetchingErrorProcessor{p}, fmm.errorProcessors...)
+	}
 }
 
-func (fmm *FetcherMiddlewareManager) Fetch(fetcherFunc func(*Request, ISpider) *FetchResult, req *Request, spider ISpider) *FetchResult {
-	var result *FetchResult
+func (fmm *FetcherMiddlewareManager) Fetch(fetchFunc func(*Request, ISpider) (*Response, error), request *Request, spider ISpider) (*Response, *Request, error) {
+	var rep *Response
+	var req *Request
+	var err error
 
-	iter := fmm.callHandlerIterator("ProcessRequest", req, spider)
-	for iter.HasNext() {
-		result = iter.Next().(*FetchResult)
-		if !result.Empty() {
+	var none = true
+	for _, processor := range fmm.requestProcessors { // chain handles request
+		rep, req, err = processor.ProcessRequest(request, spider)
+		if rep != nil || req != nil || err != nil {
+			none = false
 			break
 		}
 	}
 
-	if result.Empty() {
-		result = fetcherFunc(req, spider)
+	if none {
+		rep, err = fetchFunc(request, spider) // really fetch the request
+		assert(rep != nil || err != nil)
 	}
 
-	if result.error != nil {
-		iter = fmm.callHandlerIterator("ProcessException", result, spider)
-		for iter.HasNext() {
-			result = iter.Next().(*FetchResult)
-			if !result.Empty() {
+	if err != nil {
+		for _, processor := range fmm.errorProcessors { // chain handles error
+			rep, req = processor.ProcessError(err, request, spider)
+			if rep != nil || req != nil {
+				err = nil // ProcessError eliminated error
 				break
 			}
 		}
 	}
 
-	if result.error == nil {
-		iter = fmm.callHandlerIterator("ProcessResponse", result, spider)
-		for iter.HasNext() {
-			result = iter.Next().(*FetchResult)
-			if !result.Empty() {
-				break
-			}
-			if result.Request != nil {
-				return result
-			}
-		}
+	assert(rep != nil || req != nil || err != nil)
+
+	if err != nil {
+		return nil, nil, err
 	}
-	return result
+
+	if req != nil {
+		return nil, req, nil // reschedule the request
+	}
+
+	for _, processor := range fmm.responseProcessors { // chain handles response
+		rep, req, err = processor.ProcessResponse(rep, request, spider)
+		if req != nil {
+			return nil, req, nil // reschedule the request
+		}
+		if err != nil {
+			return nil, nil, err
+		}
+		assert(rep != nil)
+	}
+
+	return rep, nil, nil
 }
