90b032fa8e6c99cea25b431afbcbdc7e0461494b
RideWindX <ridewindx@163.com>
2017-03-30 20:49:29 +0800

    Create util.go

---
+++ util.go
@@ -0,0 +1,33 @@
+package spy
+
+import "fmt"
+
+type Opener interface {
+	Open(spider ISpider)
+}
+
+type Closer interface {
+	Close(spider ISpider)
+}
+
+func open(spider ISpider, openers ...interface{}) {
+	for i := 0; i < len(openers); i++ {
+		if opener, ok := openers[i].(Opener); ok {
+			opener.Open(spider)
+		}
+	}
+}
+
+func close(spider ISpider, closers ...interface{}) {
+	for i := len(closers)-1; i >= 0; i-- {
+		if closer, ok := closers[i].(Closer); ok {
+			closer.Close(spider)
+		}
+	}
+}
+
+func assert(value bool, formatAndArgs ...interface{}) {
+	if !value {
+		panic(fmt.Sprintf(formatAndArgs))
+	}
+}
